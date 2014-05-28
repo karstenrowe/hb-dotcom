071afcc3f61255a7f2065717cd6cc97d110a1bf4
Karsten Rowe <krowe@nutshell.com>
2014-05-28 18:56:04 +0100

    Add price list and contact form

---
+++ page/contact-us/contactthanks.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="author" href="../../humans.txt">
+    <meta name="description" content="Harrogate Health & Beauty Cosmetic Clinic. A medical cosmetic clinic specialising in advanced aesthetics and beauty treatments.">
+    <meta name="keywords" content="advanced aesthetics, beauty treatments, botox, dermal fillers, juvederm, hair removal, skin peels, meso injections, anti-ageing treatments, Harrogate, North Yorkshire, Yorkshire">
+    <link rel="shortcut icon" href="../../ico/favicon.png">
+
+    <title>Contact us: Harrogate Health & Beauty</title>
+
+    <!-- Google Fonts import -->
+		<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic|Noto+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+    <!-- @karstenrowe theme for Harrogate Health & Beauty -->
+    <link href="../../css/styles.css" rel="stylesheet">
+
+		<!-- Font Awesome icons -->
+		<link rel="stylesheet" href="../../resource/font-awesome-4.0.3/css/font-awesome.min.css">
+
+		<!-- Google analytics code -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-15109235-11', 'harrogatehealthandbeauty.com');
+		  ga('require', 'displayfeatures');
+		  ga('send', 'pageview');
+		</script>
+  </head>
+
+  <body>
+  	<div class="container">
+			<h1>Your message has been sent!</h1>
+			<p><a href="index.html">Back to website</a></p>
+		</div>
+	</body>
+</html>
