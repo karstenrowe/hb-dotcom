e541e360c8a747a7b827958d63995074248f2f15
Karsten Rowe <krowe@nutshell.com>
2014-06-03 18:29:03 +0100

    1/2 implemented contact form

--- page/contact-us/contactthanks.php
+++ page/contact-us/contactthanks.php
@@ -37,5 +37,12 @@
 			<h1>Your message has been sent!</h1>
 			<p><a href="index.html">Back to website</a></p>
 		</div>
+
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
 	</body>
 </html>
